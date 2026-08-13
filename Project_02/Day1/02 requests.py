543f677a0d85aa0071a4505a36b2043774dea1ad
Sun-0810-yh <sun0810yuhan@gmail.com>
2026-08-14 03:52:47 +0800

    更新Project_02 Day1的文件

--- Project_02/Day1/02 requests.py
+++ Project_02/Day1/02 requests.py
@@ -1,10 +1,10 @@
-# import requests
-#
-# response = requests.get("https://httpbin.org/get")
-#
-# print(response.status_code)#status_code表示HTTP响应状态码(requests库中的一个属性)
-# print(response.json())#是 requests 帮你把 HTTP 响应中的 JSON 数据解析成 Python 对象。
-#
+import requests
+
+response = requests.get("https://httpbin.org/get")
+
+print(response.status_code)#status_code表示HTTP响应状态码(requests库中的一个属性)
+print(response.json())#是 requests 帮你把 HTTP 响应中的 JSON 数据解析成 Python 对象。
+
 # #常见状态码
 # 200 → 成功
 # 404 → 找不到资源
--- Project_02/Day1/03 Git与Github.py
+++ Project_02/Day1/03 Git与Github.py
@@ -1,8 +1,16 @@
-git init #创建项目
-git status #查看状态
-git add . #把文件加入暂存区：
-git commit -m "day1: learn http and json" #创建一次版本记录
+# # Git基础命令
+# git init  #创建项目
+# git status  #查看状态
+# git add .  #把文件加入暂存区：
+# git commit -m "day1: learn http and json" #创建一次版本记录
 
-#以后修改代码
-git add .
-git commit -m "day1: add api demo"
+# #以后修改代码
+# git add .
+# git commit -m "day1: add api demo"
+
+# .gitignore  # 仓库里哪些文件不受管理
+#             # 在根目录下创建.gitignore文件 里面写不被管理的文件名 要是整个目录的话写法为:目录名/
+#             # 尽量初始化的时候就创建好
+
+# git reset --hard<comit ID>#把仓库强制回退某个版本提交
+# git revert <comit ID>#生成一个反向提交，抵消掉某次的历史提交
