2257f9e21a08df84a12a6d55b61befca86022b00
Sun-0810-yh <sun0810yuhan@gmail.com>
2026-08-15 02:28:27 +0800

    新增了API_search API查询的小项目，补充了02的知识点

--- Project_02/Day1/02 requests.py
+++ Project_02/Day1/02 requests.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get("https://httpbin.org/get")
+response = requests.get("网站地址")
 
 print(response.status_code)#status_code表示HTTP响应状态码(requests库中的一个属性)
 print(response.json())#是 requests 帮你把 HTTP 响应中的 JSON 数据解析成 Python 对象。
@@ -13,8 +13,70 @@ print(response.json())#是 requests 帮你把 HTTP 响应中的 JSON 数据解�
 # 403 → 没有权限
 
 import requests
-response =  requests.get("https://httpbin.org/get")
 
-print(response.status_code)
+response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+
+print("状态码：", response.status_code)
+print("URL：", response.url)
+print("响应内容：", response.text)
+
+data = response.json()
+
+print(data)
+print(type(data))
+
+print(data["title"])
+print(data["completed"])
+
+"https://example.com/search?keyword=Python"
+" ?keyword=Python " #表示查询参数
+
+#?keyword=Python == 下面这种params写法
+params = {
+    "keyword": "Python"
+}
+
+response = requests.get(
+    "https://httpbin.org/get",
+    params=params
+)
+
+"?keyword=Python&page=2&limit=10"
+"keyword = Python"
+"page = 2"
+"limit = 10"
+
+#理解get请求:
+import requests
+
+params = {
+    "userId": 1
+}
+
+response = requests.get(
+    "https://jsonplaceholder.typicode.com/todos",
+    params=params
+)
+
 print(response.url)
-print(response.json())
+print(response.status_code)
+
+data = response.json()
+
+print(data)
+print(type(data))
+print(len(data))
+print(data[0])
+
+#理解post请求
+import requests
+
+data = {
+    "name": "顺顺",
+    "age": 20
+}
+response = requests.post("https://jsonplaceholder.typicode.com/posts", json=data)
+
+print("状态码：",response.status_code)
+print("响应内容",response.text)
+print("响应类型",type(response.json()))
---
+++ Project_02/Day1/02_Project_API_searh.py
@@ -0,0 +1,116 @@
+#项目要求：
+
+# API 查询器 v1
+
+# 运行效果：
+# ====== API 查询器 ======
+#
+# 1. 查询 Todo
+# 2. 创建 Todo
+# 3. 退出
+
+
+# 请选择：
+# 选择 1：
+
+# 请输入用户ID：1
+
+# 请求成功！
+# 请输入用户ID：1
+
+# 请求成功！
+# 状态码：200
+#
+# 找到 10 条 Todo
+
+# 第一条：
+# 标题：delectus aut autem
+# 完成状态：False
+
+# 选择 2：
+
+# 请输入标题：学习Agent
+# 请输入用户ID：1
+#
+# 创建成功！
+# 状态码：201
+# 服务器返回：
+# {
+#     ...
+# }
+
+
+# 这里我们使用：
+# https://jsonplaceholder.typicode.com/todos
+
+# 查询参数：
+# params = {
+#     "userId": 用户输入的ID
+# }
+# 然后：
+# requests.get(...)
+
+
+import requests
+
+def search_todo():
+    user_id = int(input("请输入用户ID:"))
+    params = {
+        "userId": user_id
+    }
+
+    response = requests.get("https://jsonplaceholder.typicode.com/todos",params = params)
+
+    if response.status_code == 200:
+        print("请求成功!")
+        print("状态码:", response.status_code)
+    else:
+        print("请求失败")
+        print("状态码:", response.status_code)
+        return
+
+    data = response.json()
+    print(f"找到{len(data)}条Todo")
+
+    print("第一条:")
+    print("标题：",data[0]["title"])
+    print("完成状态：",data[0]["completed"])
+
+def send_todo():
+    title = input("请输入标题：")
+    user_id = int(input("请输入用户ID:"))
+
+    data = {
+        "title": title,
+        "userId": user_id
+    }
+
+    response = requests.post("https://jsonplaceholder.typicode.com/todos",json = data)
+    if response.status_code == 201:
+        print("创建成功!")
+        print("状态码:", response.status_code)
+    else:
+        print("创建失败")
+        print("状态码:", response.status_code)
+        return
+
+    print("服务器返回：",data)
+
+
+while True:
+    print("====== API 查询器 ======")
+    print("1.查询 Todo")
+    print("2.创建 Todo")
+    print("3.退出")
+
+    choice = int(input("请选择："))
+
+    if choice == 1:
+        search_todo()
+    elif choice == 2:
+        send_todo()
+    elif choice == 3:
+        print("感谢使用，再见!")
+        break
+    else:
+        print("输入无效,请重新输入")
